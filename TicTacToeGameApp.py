5c5f242399308b4b33f86540f16f04f46e8d7208
KhateraAlizada <khatera.alizada@gmail.com>
2023-03-04 09:34:53 -0500

    app amended

--- TicTacToeGameApp.py
+++ TicTacToeGameApp.py
@@ -1,11 +1,16 @@
 def main():
-    size = input("Enter the size of board game. \n")
-    print("\nSize of board game is " + size + "\n")
-    searchStrategyO = input("Enter legal moves, alpha-beta, legal moves or minimax legal moves strategy for O. \n")
-    searchStrategyX = input("Enter legal moves, alpha-beta, legal moves or minimax legal moves strategy for X. \n")
+    response = ""
+    while response != "No":
+        size = input("Enter the size of board game. \n")
+        print("\nSize of board game is " + size + "\n")
+        searchStrategyO = input("Enter random, alpha, or minimax legal moves strategy for O. \n")
+        searchStrategyX = input("Enter random, alpha, or minimax legal moves strategy for X. \n")
 
-    print("\nSearch strategy for O is " + searchStrategyO + "\n")
-    print("\nSearch strategy for X is " + searchStrategyX + "\n")
+        print("\nSearch strategy for O is " + searchStrategyO + "\n")
+        print("\nSearch strategy for X is " + searchStrategyX + "\n")
+
+        response = input("Would you like to run the game again? ('Yes' or 'No') .\n> ")
+    print("Thank you for Playing Our Game.")
 
 
 if __name__ == "__main__":
