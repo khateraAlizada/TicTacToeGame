e57e95da192215ff63790b996042720815f0a929
KhateraAlizada <khatera.alizada@gmail.com>
2023-03-04 09:19:33 -0500

    modified

---
+++ TicTacToeGameApp.py
@@ -0,0 +1,13 @@
+def main():
+    size = input("Enter the size of board game. \n")
+    print("\nSize of board game is " + size + "\n")
+    searchStrategyO = input("Enter legal moves, alpha-beta, legal moves or minimax legal moves strategy for O. \n")
+    searchStrategyX = input("Enter legal moves, alpha-beta, legal moves or minimax legal moves strategy for X. \n")
+
+    print("\nSearch strategy for O is " + searchStrategyO + "\n")
+    print("\nSearch strategy for X is " + searchStrategyX + "\n")
+
+
+if __name__ == "__main__":
+    main()
+
